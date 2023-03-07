a26a57ef3b1a12742afb633208e35ab47adb2b49
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-03-07 09:41:54 -0500

    Added 2nd S3 bucket and an output

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as s3,
+    CfnOutput,
     Stack,
 )
 from constructs import Construct
@@ -18,4 +19,17 @@ class MyFirstCdkProjectStack(Stack):
             versioned=False,
             encryption=s3.BucketEncryption.S3_MANAGED,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+        )
+
+        my_bucket = s3.Bucket(
+            self,
+            "myBucketId1",
+        )
+
+        output_1 = CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=my_bucket.bucket_name,
+            description="My first CDK Bucket",
+            export_name="myBucketOutput1",
         )
